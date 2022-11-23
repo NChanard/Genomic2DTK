8a41b64a2376ad13ff6bace05ca36388ae3f73d9
NChanard <nchanard@univ-tlse3.fr>
2022-11-23 13:16:17 +0100

    Add Beaf32_Peaks.gnr
    Former-commit-id: 087c2256886eb3892130be1c85eed71c8477c664

---
+++ R/Beaf32_Peaks.gnr.R
@@ -0,0 +1,16 @@
+#' Drosophila Melanogaster Beaf32 peaks on 2L and 2R chromosomes.
+#'
+#' @docType data
+#'
+#' @usage data(Beaf32_Peaks.gnr)
+#'
+#' @format An object of class GRanges.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(Beaf32_Peaks.gnr)
+#' Beaf32_Peaks.gnr
+"Beaf32_Peaks.gnr"
